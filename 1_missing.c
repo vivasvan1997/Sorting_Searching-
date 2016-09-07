6745d1abb67f21aa56411ef0e7f61316ae50c011
vivasvan1997 <vivaswan1997@gmail.com>
2016-09-07 20:21:24 +0530

    Update 1_missing.c

--- 1_missing.c
+++ 1_missing.c
@@ -1,4 +1,7 @@
 
+/* User enters the numbers from 1 to n in an array(without any repetations) of size n-1, the program
+   will give the missing number as the output */
+
 #include<stdio.h>
 
 void input(int a[], int n)
@@ -15,10 +18,10 @@ int main()
 	FILE *fp;
    	fp=freopen("read.txt","r",stdin);
 	scanf("%d",&n);
-	input(a,n);
+	input(a,n-1);
 	fclose(fp);
 	sum=n*(n+1)/2;
-	for(i=0;i<n;i++)
+	for(i=0;i<n-1;i++)
 		sum_a+=a[i];
 	fp=freopen("write.txt","w",stdout);
 	printf("\n  %d",(sum-sum_a));
